8c4a889fa9e1253a6d9b89921ec599064fb185cb
zjm <zjmwlx@126.com>
2017-05-28 21:49:14 +0800

    eclipse第一提交测试

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.SliderUI;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("helloword");
+		
+		//±‡¬Î∏Ò Ω		
+	}
+}
